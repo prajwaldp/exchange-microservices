c2a1ff1b2000e00ec6f5741e904ca6b6dff153c4
Prajwal Prakash <prajwalpy6@gmail.com>
2020-09-26 13:50:21 -0400

    Remove unused import

--- microservices-clickstream-analytics/clickstream-simulator/src/main/java/clickstreamproducer/App.java
+++ microservices-clickstream-analytics/clickstream-simulator/src/main/java/clickstreamproducer/App.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Scanner;
-import java.util.Map.Entry;
 
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
