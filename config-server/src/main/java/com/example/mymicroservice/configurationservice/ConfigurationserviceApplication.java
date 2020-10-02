846ea6d95fafd8f9df0c3122056ea3034636d1a4
Prajwal Prakash <prajwalpy6@gmail.com>
2020-10-02 06:48:00 -0400

    Rename services, add configuration server

---
+++ config-server/src/main/java/com/example/mymicroservice/configurationservice/ConfigurationserviceApplication.java
@@ -0,0 +1,15 @@
+package com.example.mymicroservice.configurationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigurationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationserviceApplication.class, args);
+	}
+
+}
---
+++ config-server/src/test/java/com/example/mymicroservice/configurationservice/ConfigurationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymicroservice.configurationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
