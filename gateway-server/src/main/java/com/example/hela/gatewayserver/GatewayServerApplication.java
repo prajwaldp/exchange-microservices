6ff91e29f17eabbc66484090786473363f498d28
Prajwal Prakash <prajwalpy6@gmail.com>
2020-10-03 00:16:25 -0400

    Add gateway server

---
+++ gateway-server/src/main/java/com/example/hela/gatewayserver/GatewayServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.hela.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+}
