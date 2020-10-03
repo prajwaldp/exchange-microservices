b66419cefc166fe63e6e2a18600cd5aeeac0bb3c
Prajwal Prakash <prajwalpy6@gmail.com>
2020-10-03 19:34:56 -0400

    Change all config to yml

--- config-server/src/main/java/configserver/ConfigserverApplication.java
+++ config-server/src/main/java/configserver/ConfigserverApplication.java
@@ -1,4 +1,4 @@
-package com.example.mymicroservice.configurationservice;
+package configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigurationserviceApplication {
+public class ConfigserverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigurationserviceApplication.class, args);
+		SpringApplication.run(ConfigserverApplication.class, args);
 	}
 
 }
